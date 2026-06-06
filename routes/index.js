5c099c8ee9c1f46173cefc084ed79e912e79e0ca
dracoon6 <86674590+dracoon6@users.noreply.github.com>
2026-06-06 09:14:36 -0600

    small fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-// Middleware to check if the user is authenticated
 const isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
